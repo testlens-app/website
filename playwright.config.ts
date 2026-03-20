673754142aed5e384d11173c0e360638d79073b3
Marc Philipp <mail@marcphilipp.de>
2026-03-20 10:59:07 +0100

    test: use built site to remove astro dev toolbar from screenshots

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   ],
 
   webServer: {
-    command: "npm run dev",
+    command: "npm run build && npm run preview",
     url: "http://localhost:4321",
     reuseExistingServer: !process.env.CI,
   },
