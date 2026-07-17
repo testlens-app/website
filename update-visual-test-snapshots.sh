9a13036512e8dad8870db9c18c5f3d9d236ddb24
Marc Philipp <mail@marcphilipp.de>
2026-07-17 13:47:32 +0000

    chore: remove unnecessary shell script
    One can simply run `./run-visual-tests.sh --update-snapshots`

--- update-visual-test-snapshots.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-for file in test-results/visual-regression-Visual-R*/*-actual.png;
-do
-  cp $file "tests/visual-regression.spec.ts-snapshots/$(basename "${file/actual/firefox-linux}")"
-done
-
